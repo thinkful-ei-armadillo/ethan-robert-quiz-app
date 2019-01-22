e44f9084d281abf3852d4677da4074fd9154fc72
bobnearents <bobnearents@yahoo.com>
2019-01-22 10:00:12 -0800

    added questions array

---
+++ mockups/questions.js
@@ -0,0 +1,73 @@
+const questions = [
+  {
+    prompt: 'What is the name of Bart\'s best friend?',
+    choices: [
+      'Milhouse',
+      'Otto',
+      'Jimbo',
+      'Karl'
+    ],
+    correctIndex: 0
+  },
+  {
+    prompt: 'What does the J stand for in Homer J Simpson?',
+    choices: [
+      'Joseph',
+      'Jeremy',
+      'Jay',
+      'James'
+    ],
+    correctIndex: 2
+  },
+  {
+    prompt: 'What is Springfield\'s neighbouring rival town called?',
+    choices: [
+      'Greensville',
+      'Shelbyville',
+      'Poolville',
+      'Dirksville'
+    ],
+    correctIndex: 1
+  },
+  {
+    prompt: 'Where did Homer and Marge get married?',
+    choices: [
+      'First Church of Springfield',
+      'Divine Madness Wedding Fantasy Chapel',
+      'Springfield Drive-Thru Marriage Parlour',
+      'Shotgun Pete\'s 24 Hour Wedding Chapel'
+    ],
+    correctIndex: 3
+  },
+  {
+    prompt: 'On what stree do the Simpsons live?',
+    choices: [
+      'Evergreen Terrace',
+      'St. Springfield Lane',
+      'Maplewood Crest',
+      'Soundbeach Ave'
+    ],
+    correctIndex: 0
+  },
+  {
+    prompt: 'What is Marge Simpsons maiden name?',
+    choices: [
+      'Carlson',
+      'Bouiver',
+      'Treado',
+      'Burns'
+    ],
+    correctIndex: 1
+  },
+  {
+    prompt: 'Who is Mr. Burns personal assisstant',
+    choices: [
+      'Homer',
+      'Apu',
+      'Smithers',
+      'Lenny'
+    ],
+    correctIndex: 2
+  },
+]
+
